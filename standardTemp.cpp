9974039a24c67b34be041516af09cf57a587687b
Archaic-Mage <64400453+Archaic-Mage@users.noreply.github.com>
2020-12-04 14:56:52 +0530

    Add files via upload

---
+++ standardTemp.cpp
@@ -0,0 +1,37 @@
+/*  
+           ____    __                    __
+     /\   |  __>  /   |   |    /\   |   /
+    /  \  |  \   <    |---|   /  \  |  <
+   /    \ |   \   \__ |   |  /    \ |   \__
+
+         _   __            ____   ___
+        | \_/ |   /\     /       |
+        |     |  /  \   <  <-->  |---
+        |     | /    \   \__\ |  |___
+
+
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef int64_t i64;
+typedef unsigned long long ull;
+#define f(i, a, b) for (int i= a; i < b; i++)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+#define pcc pair<char, char>
+#define vpii vector<pii>
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<ll>
+
+using namespace std;
+
+int main() {
+
+
+    
+    return 0;
+}
