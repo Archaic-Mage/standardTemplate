09f6ca1d742d219140d6c40ebcc1ec41c885c024
Archaic-Mage <sohasoham811@gmail.com>
2021-01-08 10:05:54 +0530

    modifications

--- standardTemp.cpp
+++ standardTemp.cpp
@@ -9,6 +9,21 @@
         |     |   /  \   <  <-->   |---
         |     |  /    \   \__\ |   |___
 
+  Author::::::::: Archaic_Mage :::::::::Author
+
+     _____    __________  _________     ___ ___      _____    .___  _________  
+   /  _  \   \______   \ \_   ___ \   /   |   \    /  _  \   |   | \_   ___ \ 
+  /  /_\  \   |       _/ /    \  \/  /    ~    \  /  /_\  \  |   | /    \  \/ 
+ /    |    \  |    |   \ \     \____ \    Y    / /    |    \ |   | \     \____
+ \____|__  /  |____|_  /  \______  /  \___|_  /  \____|__  / |___|  \______  /
+         \/          \/          \/         \/           \/                \/ 
+               _____       _____      ________  ___________                  
+              /     \     /  _  \    /  _____/  \_   _____/                  
+             /  \ /  \   /  /_\  \  /   \  ___   |    __)_                   
+            /    Y    \ /    |    \ \    \_\  \  |        \                  
+            \____|__  / \____|__  /  \______  / /_______  /                  
+                    \/          \/          \/          \/                   
+
 
 */
 
@@ -17,15 +32,20 @@
 typedef long long ll;
 typedef int64_t i64;
 typedef unsigned long long ull;
+
 #define f(i, a, b) for (ll i= a; i < b; i++)
+#define fd(i, a, b) for(ll i = a; i >= b; i--)
+#define pb push_back
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 #define pss pair<string, string>
 #define pcc pair<char, char>
+#define p pair
 #define vpii vector<pii>
 #define vi vector<int>
 #define vc vector<char>
 #define vll vector<ll>
+#define v vector
 
 using namespace std;
 
