c11c73a113c952baa975daafd262d9fda36eb839
Soham Tripathy <64400453+Archaic-Mage@users.noreply.github.com>
2022-07-24 16:03:35 +0530

    Add DSU - implementation

--- standardTemp.cpp
+++ standardTemp.cpp
@@ -30,7 +30,6 @@
 #include <bits/stdc++.h>
 
 typedef long long ll;
-typedef int64_t i64;
 typedef unsigned long long ull;
 
 #define f(i, a, b) for (ll i= a; i < b; i++)
@@ -40,16 +39,58 @@ typedef unsigned long long ull;
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 #define pss pair<string, string>
-#define pcc pair<char, char>
-#define p pair
-#define vpii vector<pii>
 #define vi vector<int>
-#define vc vector<char>
 #define vll vector<ll>
-#define v vector
+#define vec vector
 #define endl "\n"
 
 using namespace std;
+
+/** DSU - Disjoint Set Union **/
+
+// class DSU {
+//     int* parent;
+//     int* rank;
+  
+// public:
+//     DSU(int n)
+//     {
+//         parent = new int[n];
+//         rank = new int[n];
+  
+//         for (int i = 0; i < n; i++) {
+//             parent[i] = -1;
+//             rank[i] = 1;
+//         }
+//     }
+  
+//     // Find function
+//     int find(int i)
+//     {
+//         if (parent[i] == -1)
+//             return i;
+  
+//         return parent[i] = find(parent[i]);
+//     }
+//     // union function
+//     void unite(int x, int y)
+//     {
+//         int s1 = find(x);
+//         int s2 = find(y);
+  
+//         if (s1 != s2) {
+//             if (rank[s1] < rank[s2]) {
+//                 parent[s1] = s2;
+//                 rank[s2] += rank[s1];
+//             }
+//             else {
+//                 parent[s2] = s1;
+//                 rank[s1] += rank[s2];
+//             }
+//         }
+//     }
+// };
+  
 
 void solve() {
 	
